ad6b1c9e415b78cc1d1d90cd698178d3ddf9d94f
xuyong <xuyong@qianmi.com>
2017-07-07 10:22:02 +0800

    python-0-basic-number-character

---
+++ Python-0-basic/008_number.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+
+# http://www.runoob.com/python/python-numbers.html
+# Python Number 数据类型用于存储数值。
+# 数据类型是不允许改变的,这就意味着如果改变 Number 数据类型的值，将重新分配内存空间。
+
+var1 = 1
+var2 = 10
+
+print var1, var2,
+
+print complex(var1, var2);
+
+# print cmp(x, y); 如果 x < y 返回 -1, 如果 x == y 返回 0, 如果 x > y 返回 1
+print cmp(1, 3);
+print cmp(1, 1);
+print cmp(6, 3);
---
+++ Python-0-basic/009_character-string.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+# 字符串是 Python 中最常用的数据类型。我们可以使用引号('或")来创建字符串。
+
+var1 = 'Hello World!';
+var2 = 'Python Runoob - 001';
+
+print "var1[0]: ", var1[0]
+print "var2[0:6]: ", var2[0:6]
+
+print "更新字符串 :- ", var1[:6] + 'Runoob!'
+
+print var2.split('-');
