1b52791ac104d6b3d592f200fb5cca058abcfb44
xuyong <xuyong@qianmi.com>
2017-07-07 19:43:00 +0800

    python-0-basic-dictionary

---
+++ Python-0-basic/012_dictionary.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+# Dictionary
+
+# 字典是另一种可变容器模型，且可存储任意类型对象。
+# 字典的每个键值(key=>value)对用冒号(:)分割，每个对之间用逗号(,)分割，整个字典包括在花括号({})中 ,格式如下所示：
+# d = {key1: value1, key2: value2}
+
+dict = {'Alice': '2341', 'Beth': '9102', 'Cecil': '3258'}
+print 'dict-->>', dict;
+print "dict['Alice']-->>", dict['Alice'];
+print;
+
+dict1 = {'aa': 'aa', 'a': 'a', 'bb': 'bb', 'ccc': 'ccc'};
+print 'dict1-->>', dict1;
+print "dict1['a']-->>", dict1['a'];
+dict1['d'] = 'd';
+print 'dict1-->>', dict1;
+
+
