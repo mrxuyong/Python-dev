0481e7020fdd7b2e632f00fc223b6e178b085141
mrxuyong <lovingPython@qq.com>
2023-07-09 16:07:17 +0800

    python"文件批量重命名"工具，代码整理

--- 3-my-tools/tools-001.py
+++ 3-my-tools/tools-001.py
@@ -10,24 +10,27 @@
 
 import os
 
-path = input('请输入文件路径(结尾加上/)：')  # /Volumes/XCJLY64G/2023-南洋女儿情/
+# /Volumes/XCJLY64G/2023-南洋女儿情/
+# /Users/xuyong/Downloads/0_download_temp0/2023-南洋女儿情/
+path = input('请输入文件路径(结尾加上/)：')
 
 # 获取该目录下所有文件，存入列表中
 f = os.listdir(path)
 
 n = 0
 for i in f:
-    print("f" + n, "====>>", f[n]);  # 南洋女儿情第35集.mp4 。。。
+    # print(n, "==>>", f[n]);  # 南洋女儿情第35集.mp4 。。。
 
-    # 旧文件名（就是路径+文件名）
+    # 旧文件名（路径+文件名）
     oldName = path + f[n]
 
     # 设置新文件名，[某个文字替换|添加某个字符 等等]
-    newName = path + f[n].replace("第", '-')
+    newName = path + f[n].replace("集", '')
+    # newName = path + f[n].replace('女儿情', '')
 
-    print(oldName, '====>>', newName)
+    print(oldName, '====>>\n', newName, '\n')
     # 用os模块中的rename方法对文件改名
-    # os.rename(oldName, newName)
+    os.rename(oldName, newName)
     n += 1
 
 
